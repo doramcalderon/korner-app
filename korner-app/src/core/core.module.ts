35a5e734729e4f5a09b3734adc5efe1c404efbb7
Adoracion Calderon <acalderonriv@csc.com>
2019-04-14 21:38:53 +0200

    chore: add Angular HttpClientModule

--- korner-app/src/core/core.module.ts
+++ korner-app/src/core/core.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { ApiInterceptor } from './http/api.interceptor';
 
 @NgModule({
+	imports: [
+		HttpClientModule,
+	],
 	providers: [
 		{ provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true },
 	]
