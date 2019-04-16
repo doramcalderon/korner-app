c246434f505a2d559a9c7a5763500007dbc356f5
Adoracion Calderon <acalderonriv@csc.com>
2019-04-16 22:07:07 +0200

    feat: use of the translate pipe in the Bet component

--- korner-app/src/pages/home/home.component.module.ts
+++ korner-app/src/pages/home/home.component.module.ts
@@ -4,18 +4,20 @@ import { IonicModule } from 'ionic-angular';
 import { BettingListComponent, BetComponent } from './components';
 import { HomePage } from './home';
 import { BettingListService } from './components/betting-list.service';
+import { CoreModule } from '../../core/core.module';
 
 @NgModule({
     imports: [
-        IonicModule.forRoot(HomePage)
+        IonicModule.forRoot(HomePage),
+        CoreModule,
     ],
     declarations: [
         HomePage,
-        BettingListComponent,        
+        BettingListComponent,
         BetComponent
     ],
     entryComponents: [
-        HomePage,        
+        HomePage,
     ],
     providers: [
         BettingListService
