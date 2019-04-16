c814ee2771768b74fa4a16fd630bf04eb4b6d99e
Adoracion Calderon <acalderonriv@csc.com>
2019-04-16 22:05:18 +0200

    chore: configure the TranslateModule

--- korner-app/src/core/core.module.ts
+++ korner-app/src/core/core.module.ts
@@ -1,14 +1,35 @@
 import { NgModule } from '@angular/core';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader, MissingTranslationHandler } from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 import { ApiInterceptor } from './http/api.interceptor';
+import { MyMissingTranslationHandler } from './missing-translation-handler.service';
+
+export function createTranslateLoader(http: HttpClient) {
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
 	imports: [
 		HttpClientModule,
+		TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: (createTranslateLoader),
+                deps: [HttpClient]
+            },
+            missingTranslationHandler: {
+                provide: MissingTranslationHandler,
+                useClass: MyMissingTranslationHandler,
+            }
+        }),
 	],
 	providers: [
 		{ provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true },
+	],
+	exports: [
+		TranslateModule,
 	]
 })
 export class CoreModule {
