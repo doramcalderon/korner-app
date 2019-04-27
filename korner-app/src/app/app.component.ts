9c9625b096153d24d73bfa5269ffc98a15859f77
Adoracion Calderon <acalderonriv@csc.com>
2019-04-27 20:11:03 +0200

    feat(app): set the Login page as the main one

--- korner-app/src/app/app.component.ts
+++ korner-app/src/app/app.component.ts
@@ -5,14 +5,14 @@ import { Platform } from 'ionic-angular';
 
 import { TranslateService } from '@ngx-translate/core';
 
-import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
 
 
 @Component({
 	templateUrl: 'app.html',
 })
 export class MyApp {
-	rootPage: any = TabsPage;
+	rootPage: any = LoginPage;
 
 	constructor(
 		platform: Platform,
