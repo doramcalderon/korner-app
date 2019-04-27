502e16aafaf014c2c378cf62e7d538f143ba28eb
Adoracion Calderon <acalderonriv@csc.com>
2019-04-27 22:20:51 +0200

    feat(auth): get token endpoint

--- korner-api/src/auth/auth.controller.ts
+++ korner-api/src/auth/auth.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+
+import { SignInDto } from './dto/signin.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+	constructor(private readonly authService: AuthService) {}
 
+	@Post('token')
+	signIn(@Body() signinDto: SignInDto): Promise<string> {
+		return this.authService.signIn(signinDto);
+	}
 }
