f0da6c35117669e1c65c64e8b421856318955a57
Adoracion Calderon <acalderonriv@csc.com>
2019-04-27 22:52:47 +0200

    feat(login): use of reactive forms

--- korner-app/src/pages/login/login.ts
+++ korner-app/src/pages/login/login.ts
@@ -1,9 +1,21 @@
 import { Component } from "@angular/core";
+import { FormGroup, FormBuilder } from "@angular/forms";
 
 @Component({
 	selector: 'page-login',
 	templateUrl: 'login.html',
 })
 export class LoginPage {
+	formGroup: FormGroup;
 
+	constructor(private formBuilder: FormBuilder) {}
+
+	ngOnInit() {
+		this.formGroup = this.formBuilder.group({
+			username: [''],
+			password: [''],
+		});
+	}
+
+	onSubmit() {}
 }
