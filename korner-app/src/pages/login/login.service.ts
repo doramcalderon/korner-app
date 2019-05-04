621c33d6518973ba2c77bc18163dd6f02c08e870
Adoracion Calderon <acalderonriv@csc.com>
2019-05-04 21:10:14 +0200

    feat(login): store the api token in the local storage

--- korner-app/src/pages/login/login.service.ts
+++ korner-app/src/pages/login/login.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 import { Observable } from 'rxjs';
+
+import { AuthenticationService } from '../../core';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable()
 export class LoginService {
 	private readonly createTokenUrl = '/korner-api/auth/token';
 
-	constructor(private http: HttpClient) {
-
-	}
+	constructor(
+		private http: HttpClient,
+		private auth: AuthenticationService,
+	) {}
 
 	public login(username, password): Observable<string> {
 		const body = {
 			username,
 			password,
 		};
-		return this.http.post<string>(this.createTokenUrl, body);
+		return this.http.post<string>(this.createTokenUrl, body).pipe(
+			switchMap(token => this.auth.persist(token))
+		);
 	}
 
 }
