acaa8ea727e9042133aff1f335f5e273b64c6294
Adoracion Calderon <acalderonriv@csc.com>
2019-05-05 23:07:33 +0200

    fix(core): move the TranslateModule to app module

--- korner-app/src/app/app.module.ts
+++ korner-app/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,13 +7,22 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { IonicStorageModule } from '@ionic/storage';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+import { MissingTranslationHandler, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
 import { CoreModule } from '../core/core.module';
+import { MyMissingTranslationHandler } from '../core/missing-translation-handler.service';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomeComponentModule } from '../pages/home/home.component.module';
+import { LoginModule } from '../pages/login/login.module';
 import { TabsPage } from '../pages/tabs/tabs';
 import { MyApp } from './app.component';
-import { LoginModule } from '../pages/login/login.module';
+
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -29,6 +39,17 @@ import { LoginModule } from '../pages/login/login.module';
     LoginModule,
     IonicModule.forRoot(MyApp),
     IonicStorageModule.forRoot(),
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+      },
+      missingTranslationHandler: {
+          provide: MissingTranslationHandler,
+          useClass: MyMissingTranslationHandler,
+      }
+  }),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- korner-app/src/core/core.module.ts
+++ korner-app/src/core/core.module.ts
@@ -1,30 +1,13 @@
 import { NgModule } from '@angular/core';
-import { HTTP_INTERCEPTORS, HttpClientModule, HttpClient } from '@angular/common/http';
-import { TranslateModule, TranslateLoader, MissingTranslationHandler } from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { ApiInterceptor } from './http/api.interceptor';
-import { MyMissingTranslationHandler } from './missing-translation-handler.service';
 import { AuthenticationService } from './auth/authentication.service';
-
-export function createTranslateLoader(http: HttpClient) {
-    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
 
 @NgModule({
 	imports: [
 		HttpClientModule,
-		TranslateModule.forRoot({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: (createTranslateLoader),
-                deps: [HttpClient]
-            },
-            missingTranslationHandler: {
-                provide: MissingTranslationHandler,
-                useClass: MyMissingTranslationHandler,
-            }
-        }),
 	],
 	providers: [
         AuthenticationService,
