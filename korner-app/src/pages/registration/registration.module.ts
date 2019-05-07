e44d0126fb63fc05323679644c6f3156a1d50374
Adoracion Calderon <acalderonriv@csc.com>
2019-05-07 10:58:59 +0200

    feat(registration): template design

--- korner-app/src/pages/registration/registration.module.ts
+++ korner-app/src/pages/registration/registration.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { RegistrationPage } from './registration';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { RegistrationPage } from './registration';
   ],
   imports: [
     IonicPageModule.forChild(RegistrationPage),
+    TranslateModule.forChild(),
   ],
 })
 export class RegistrationPageModule {}
