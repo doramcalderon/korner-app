d130e650c5100cc4209bda4f380044e7ff893530
Adoracion Calderon <acalderonriv@csc.com>
2019-05-07 12:40:52 +0200

    feat(registration): show validation messages

--- korner-app/src/pages/registration/registration.module.ts
+++ korner-app/src/pages/registration/registration.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { RegistrationPage } from './registration';
 import { TranslateModule } from '@ngx-translate/core';
+import { NgxErrorsModule } from '@ultimate/ngxerrors';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { TranslateModule } from '@ngx-translate/core';
   imports: [
     IonicPageModule.forChild(RegistrationPage),
     TranslateModule.forChild(),
+    NgxErrorsModule,
   ],
 })
 export class RegistrationPageModule {}
