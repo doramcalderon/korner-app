d96c33528eaeb6ded2c1c1653913958f710d38f5
Adoracion Calderon <acalderonriv@csc.com>
2019-05-16 18:23:29 +0200

    chore: configure mailer module

--- korner-api/src/app.module.ts
+++ korner-api/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MailerModule } from '@nest-modules/mailer';
 
 import { AuthModule } from 'auth/auth.module';
 import { BetModule } from 'bets/bet.module';
@@ -8,11 +9,30 @@ import { RegistrationModule } from 'registration/registration.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-
 @Module({
   imports: [
     // MongooseModule.forRoot('mongodb://localhost/nest'),
     TypeOrmModule.forRoot(),
+    MailerModule.forRoot({
+      transport: {
+          host: process.env.MAILER_HOST,
+          port: parseInt(process.env.MAILER_PORT, 10),
+          secure: process.env.MAILER_SECURE === 'true',
+          auth: {
+              user: process.env.MAILER_USER,
+              pass: process.env.MAILER_PASS,
+          },
+      },
+      defaults: {
+          from: process.env.MAILER_FROM,
+      },
+      template: {
+        dir: './src/common/email-templates',
+        options: {
+          engine: 'handlebars',
+        },
+      },
+    }),
     AuthModule,
     BetModule,
     MatchDayModule,
