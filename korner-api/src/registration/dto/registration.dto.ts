00c150fc6a743a8289aa463f1af5bdf1830a7ac9
Adoracion Calderon <acalderonriv@csc.com>
2019-05-16 17:42:58 +0200

    feat(registration): registration dto validation

--- korner-api/src/registration/dto/registration.dto.ts
+++ korner-api/src/registration/dto/registration.dto.ts
@@ -1,15 +1,21 @@
 import { ApiModelProperty } from '@nestjs/swagger';
 
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+
 export class RegistrationDto {
 	@ApiModelProperty()
+	@IsNotEmpty()
 	name: string;
 
 	@ApiModelProperty()
 	lastname: string;
 
 	@ApiModelProperty()
+	@IsEmail()
 	mail: string;
 
 	@ApiModelProperty()
+	@IsNotEmpty()
 	password: string;
 }
