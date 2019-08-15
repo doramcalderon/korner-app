d1c688022bad6f6743c8c2732bf3609befa4cb0b
Adoracion Calderon <acalderonriv@csc.com>
2019-08-15 19:47:11 +0200

    feat(home): show notification when no credit

--- korner-app/src/pages/home/home.component.module.ts
+++ korner-app/src/pages/home/home.component.module.ts
@@ -7,28 +7,13 @@ import { BettingListComponent, BetComponent } from './components';
 import { HomePage } from './home';
 import { BettingListService } from './components/betting-list.service';
 import { CoreModule } from '../../core/core.module';
+import { ProfileService } from '../profile/profile.service';
 
 @NgModule({
-    imports: [
-        IonicModule.forRoot(HomePage),
-        CoreModule,
-        AvatarModule,
-    ],
-    declarations: [
-        HomePage,
-        BettingListComponent,
-        BetComponent
-    ],
-    entryComponents: [
-        HomePage,
-    ],
-    providers: [
-        BettingListService
-    ],
-    exports: [
-        HomePage
-    ]
+    imports: [IonicModule.forRoot(HomePage), CoreModule, AvatarModule],
+    declarations: [HomePage, BettingListComponent, BetComponent],
+    entryComponents: [HomePage],
+    providers: [BettingListService, ProfileService],
+    exports: [HomePage],
 })
-export class HomeComponentModule {
-
-}
+export class HomeComponentModule {}
--- korner-app/src/pages/home/home.ts
+++ korner-app/src/pages/home/home.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { NavController, ToastController } from 'ionic-angular';
+
+import { ProfileService } from '../profile/profile.service';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html',
 })
-export class HomePage {
+export class HomePage implements OnInit {
+    constructor(
+        public navCtrl: NavController,
+        private toastCtrl: ToastController,
+        private profileService: ProfileService,
+        private translate: TranslateService,
+    ) {}
 
-  constructor(public navCtrl: NavController) {}
+    goToProfile() {
+        this.navCtrl.push('ProfilePage');
+    }
 
-  goToProfile() {
-    this.navCtrl.push('ProfilePage');
-  }
+    ngOnInit() {
+        this.profileService
+            .fetchProfile()
+            .subscribe(profile =>
+                this.showToastIfInsufficientCredit(profile.credit),
+            );
+    }
 
+    private showToastIfInsufficientCredit(currentCredit: number) {
+        if (!currentCredit) {
+            this.toastCtrl
+                .create({
+                    duration: 3000,
+                    position: 'top',
+                    message: this.translate.instant('HOME.NO_CREDIT'),
+                })
+                .present();
+        }
+    }
 }
