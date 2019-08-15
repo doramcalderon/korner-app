df102305cc232df031890b03f78ee14e79cdb7ef
Adoracion Calderon <acalderonriv@csc.com>
2019-08-15 19:13:21 +0200

    feat(profile): display the user's credit

--- korner-app/src/pages/profile/profile.model.ts
+++ korner-app/src/pages/profile/profile.model.ts
@@ -1,4 +1,5 @@
 export interface Profile {
-	name: string;
-	surname: string;
+    name: string;
+    surname: string;
+    credit: number;
 }
--- korner-app/src/pages/profile/profile.ts
+++ korner-app/src/pages/profile/profile.ts
@@ -1,51 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
 
-import { Profile } from './profile.model';
-import { ProfileService } from './profile.service';
-import { LoginService } from '../login/login.service';
-import { LoginPage } from '../login/login';
+import { Profile } from "./profile.model";
+import { ProfileService } from "./profile.service";
+import { LoginService } from "../login/login.service";
+import { LoginPage } from "../login/login";
 
 @IonicPage()
 @Component({
-  selector: 'page-profile',
-  templateUrl: 'profile.html',
+    selector: "page-profile",
+    templateUrl: "profile.html"
 })
 export class ProfilePage implements OnInit {
+    profile: Profile;
+    fullName: string;
 
-  profile: Profile;
-  fullName: string;
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        public profileService: ProfileService,
+        public loginService: LoginService
+    ) {}
 
-  constructor(
-      public navCtrl: NavController,
-      public navParams: NavParams,
-      public profileService: ProfileService,
-      public loginService: LoginService,
-  ) {}
-
-  ngOnInit() {
-    this.profileService.fetchProfile().subscribe(
-        profile => this.handleProfile(profile),
-        error => this.handleError(error),
-    )
-  }
-
-  logout() {
-    this.loginService.logout().subscribe(
-        () => this.navCtrl.push(LoginPage)
-    );
-
-  }
-
-  private handleProfile(profile: Profile) {
-    this.profile = profile;
-
-    if(profile) {
-      this.fullName = `${profile.name} ${profile.surname}`;
+    ngOnInit() {
+        this.profileService
+            .fetchProfile()
+            .subscribe(
+                profile => this.handleProfile(profile),
+                error => this.handleError(error)
+            );
     }
-  }
 
-  private handleError(error: any) {
-      console.error(`Error ${JSON.stringify(error)}`);
-  }
+    logout() {
+        this.loginService
+            .logout()
+            .subscribe(() => this.navCtrl.push(LoginPage));
+    }
+
+    private handleProfile(profile: Profile) {
+        this.profile = profile;
+
+        if (profile) {
+            this.fullName = `${profile.name} ${profile.surname}`;
+        }
+    }
+
+    private handleError(error: any) {
+        console.error(`Error ${JSON.stringify(error)}`);
+    }
 }
